512e87e08a83a18b92f086fbef5e0d9b0dbb60db
danielgsalas <danielgsalas@yahoo.com>
2015-10-25 06:24:09 -0700

    added initial "Hello World" version of app

---
+++ src/com/appstoremarketresearch/webviewpicsandvideos/controller/InfoActivity.java
@@ -0,0 +1,27 @@
+package com.appstoremarketresearch.webviewpicsandvideos.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.appstoremarketresearch.webviewpicsandvideos.R;
+
+public class InfoActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.info, menu);
+        return true;
+    }
+
+}
---
+++ src/com/appstoremarketresearch/webviewpicsandvideos/view/InfoFragment.java
@@ -0,0 +1,37 @@
+package com.appstoremarketresearch.webviewpicsandvideos.view;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.appstoremarketresearch.webviewpicsandvideos.R;
+
+public class InfoFragment extends Fragment
+{
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater,
+        ViewGroup container,
+        Bundle savedInstanceState)
+    {
+        super.onCreateView(inflater, container, savedInstanceState);
+        
+        View topLevelView = inflater.inflate(R.layout.fragment_info, container, false);
+        
+        initializeWebView(topLevelView);
+        
+        return topLevelView;
+    }
+    
+    /**
+     * initializeWebView
+     */
+    private void initializeWebView(View topLevelView)
+    {
+        WebView webview = (WebView) topLevelView.findViewById(R.id.webview);
+        webview.loadUrl("file:///android_asset/html/Info.html");
+    }
+}
