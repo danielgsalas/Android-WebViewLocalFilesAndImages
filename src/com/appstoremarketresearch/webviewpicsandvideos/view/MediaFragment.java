b9b8b6d7c6e2c1092cfb27b8a95d027dcc79f6f4
danielgsalas <danielgsalas@yahoo.com>
2015-11-12 06:34:31 -0800

    split Media screen into two HTML pages

--- src/com/appstoremarketresearch/webviewpicsandvideos/view/MediaFragment.java
+++ src/com/appstoremarketresearch/webviewpicsandvideos/view/MediaFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 import com.appstoremarketresearch.webviewpicsandvideos.R;
@@ -21,18 +22,61 @@ public class MediaFragment extends Fragment
         
         View topLevelView = inflater.inflate(R.layout.fragment_media, container, false);
         
-        initializeWebView(topLevelView);
+        initializeWebViews(topLevelView);
         
         return topLevelView;
     }
     
     /**
-     * initializeWebView
+     * initializeWebViews
      */
-    private void initializeWebView(View topLevelView)
+    private void initializeWebViews(View topLevelView)
     {
-        WebView webview = (WebView) topLevelView.findViewById(R.id.webview);
-        webview.getSettings().setJavaScriptEnabled(true);
-        webview.loadUrl("file:///android_asset/html/Media.html");        
+        WebView mediaPreview = (WebView) topLevelView.findViewById(R.id.media_preview);
+        mediaPreview.getSettings().setJavaScriptEnabled(true);
+
+        WebView mediaContent = (WebView) topLevelView.findViewById(R.id.media_content);
+        mediaContent.getSettings().setJavaScriptEnabled(true);
+
+        JavaScriptInterface jsi = new JavaScriptInterface(mediaPreview, mediaContent);
+        mediaPreview.addJavascriptInterface(jsi, "android");
+        mediaContent.addJavascriptInterface(jsi, "android");
+
+        mediaPreview.loadUrl("file:///android_asset/html/MediaPreview.html");
+        mediaContent.loadUrl("file:///android_asset/html/MediaContent.html");
+    }
+    
+    /**
+     * Java-JavaScript bridge
+     */
+    final class JavaScriptInterface
+    {
+        private WebView mediaPreview;
+        private WebView mediaContent;
+        
+        public JavaScriptInterface(
+            WebView mediaPreview, 
+            WebView mediaContent) 
+        {
+            this.mediaPreview = mediaPreview;
+            this.mediaContent = mediaContent;
+        }
+        
+        @JavascriptInterface
+        public void showImage(final String imageName)
+        {            
+            // java.lang.Throwable: A WebView method was called on thread 'JavaBridge'.
+            // All WebView methods must be called on the same thread.
+            //mediaContent.loadUrl("javascript:showImage('" + imageName + "')");
+
+            // pass the request from MediaPreview.html to MediaContent.html
+            // by calling JavaScript on the Android UI thread not a background thread
+            mediaContent.post(new Runnable() {
+                @Override
+                public void run() {
+                    mediaContent.loadUrl("javascript:showImage('" + imageName + "')");
+                }
+            });
+        }
     }
 }
